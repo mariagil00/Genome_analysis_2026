a07b43d63516dfe1fe3f14f12e864d7e2c7694f2
Maria Gil Sancho <magi1969@pelle1.uppmax.uu.se>
2026-05-18 18:58:39 +0200

    Commit SAM_RNAseq_Serum

---
+++ scripts/SAM_RNAseq_Serum.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 4
+#SBATCH -t 01:00:00
+#SBATCH -J SAM_RNAseq_Serum.sh
+#SBATCH -o %x.out
+#SBATCH -e %x.err
+
+module load SAMtools/1.22.1-GCC-13.3.0
+
+SAM="/home/magi1969/Genome_analysis_2026/results/BWA_RNA_seq_Serum/ERR17979" 
+OUTDIR="/home/magi1969/Genome_analysis_2026/results/SAM_RNAseq_Serum"
+PREFIX="ERR17979"
+
+for x in {69..71}; do
+samtools view -bS ${SAM}${x}.sam > ${OUTDIR}/${PREFIX}${x}.bam
+samtools sort ${OUTDIR}/${PREFIX}${x}.bam -o ${OUTDIR}/${PREFIX}${x}_sorted.bam
+samtools index ${OUTDIR}/${PREFIX}${x}_sorted.bam
+samtools sort -n ${OUTDIR}/${PREFIX}${x}.bam -o ${OUTDIR}/${PREFIX}${x}_sorted_name.bam
+done 
